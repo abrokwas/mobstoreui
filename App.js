fbf4d2608fccb003459bc218fc4692c8aba958d7
abrokwas <tkromeo2003@gmail.com>
2021-04-25 07:05:30 +0100

    folder struct, products folder in screens update

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import ProductContainer from './Screens/Products/ProductContainer'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ProductContainer/>
     </View>
   );
 }
---
+++ Screens/Products/ProductContainer.js
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from 'react'
+import { View,Text, StyleSheet, ActivityIndicator, FlatList } from 'react-native'
+
+const data = require('../../assets/data/products.json')
+import ProductList from './ProductList'
+
+
+const ProductContainer = () => {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        setProducts(data);
+
+        return () => {
+            setProducts([])
+        }
+    }, [])
+
+    return(
+        <View>
+            <Text>Hello sam form Product container</Text>
+            <View style={styles.listContainer}>
+              {products.map((item) => {
+               return(
+                   <ProductList
+                       
+                       key={item.name}
+                       item={item}
+                   />
+               )
+           })}
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexWrap: "wrap",
+    backgroundColor: "gainsboro",
+  },
+  listContainer: {
+    
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-start",
+    flexWrap: "wrap",
+    backgroundColor: "gainsboro",
+  },
+  center: {
+      justifyContent: 'center',
+      alignItems: 'center'
+  }
+});
+
+export default ProductContainer
