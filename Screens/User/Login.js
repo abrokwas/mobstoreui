fcab2bbf496ab0e0921cfda417a330486d716f7f
abrokwas <tkromeo2003@gmail.com>
2021-07-15 14:48:07 +0100

    login n register Screens base completed

--- Screens/User/Login.js
+++ Screens/User/Login.js
@@ -1,12 +1,68 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import FormContainer from "../../Shared/Form/FormContainer";
+import Input from "../../Shared/Form/Input";
+import Error from "../../Shared/Error";
 
 const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = () => {
+    const user = {
+      email,
+      password,
+    };
+
+    if (email === "" || password === "") {
+      setError("Please fill in your credentials");
+    } else {
+      console.log("success");
+    }
+  };
+
   return (
-    <View>
-      <Text>User Login Page</Text>;
-    </View>
+    <FormContainer title={"Login"}>
+      <Input
+        placeholder={"Enter Email"}
+        name={"email"}
+        id={"email"}
+        value={email}
+        onChangeText={(text) => setEmail(text.toLowerCase())}
+      />
+      <Input
+        placeholder={"Enter Password"}
+        name={"password"}
+        id={"password"}
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+      <View style={styles.buttonGroup}>
+        {error ? <Error message={error} /> : null}
+        <Button title="Login" onPress={() => handleSubmit()} />
+      </View>
+      <View style={[{ marginTop: 40 }, styles.buttonGroup]}>
+        <Text style={styles.middleText}>Don't have an account yet?</Text>
+        <Button
+          title="Register"
+          onPress={() => props.navigation.navigate("Register")}
+        />
+      </View>
+    </FormContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  buttonGroup: {
+    width: "80%",
+    alignItems: "center",
+  },
+  middleText: {
+    marginBottom: 20,
+    alignSelf: "center",
+  },
+});
 
 export default Login;
